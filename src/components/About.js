0d209a1fcb447004c96bfc172908c5ad207c82b4
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2021-03-29 09:56:32 +0100

    changed description

--- src/components/About.js
+++ src/components/About.js
@@ -14,13 +14,12 @@ function About() {
                     about<span className="dots_title">.</span>
                 </h1>
                 <p className="about_section_text">
-                    hi. I am not a designer, I am not a developer and I am still not good at coding. I try, I learn and I keep on studying. 
+                    front end developer based in London. i am changing my career paths now. from microbiology and health and safety into coding.  
 
                     <br />
-                    Since you are here, have a look at my projects I have been working on. If you would like to collaborate, please drop me a message.
+                    since you are here, check out my projects I have been working on. If you would like to collaborate, please drop me a message.
                     <br />
-                    Peace
-                    <br />
+
                     ...and lets keep coding.
                 </p>
                 <div className="about_social_wrapper">
