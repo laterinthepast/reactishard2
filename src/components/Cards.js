49f5934b404ba9b78fba4647216205ecbbeba056
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2021-06-21 19:58:34 +0100

    link changed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -38,7 +38,7 @@ function Cards() {
                             src="/images/welcome.jpg"
                             text="Design, build and management of the website. Na ha ku is a company selling crafts online. Future project development will include setting up online ecommerce platform - React, styled components... work in progress "
                             label="Design, Build and Management"
-                            path="https://agitated-pare-e17700.netlify.app"
+                            path="https://60c889031eb7b00008a27487--agitated-pare-e17700.netlify.app/"
                             pathgithub="https://github.com/laterinthepast/quartz-website"
                         />
 
